db6acf7624a3c94bc59bec0f4ffc396ee8512a6a
Eduardo Henrique Gonçalves <eduardohenriquegoncalves12@gmail.com>
2026-09-02 19:19:38 -0300

    feat: add local and cloud WebSocket protocol

---
+++ src/WebSocket.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <ESPAsyncWebServer.h>
+#include <WebSocketsClient.h>
+#include "Alarm.h"
+#include "Logger.h"
+#include "Radar.h"
+#include "WifiManager.h"
+class RadarWebSocket {
+ public:
+  RadarWebSocket(Radar& radar, Alarm& alarm, Logger& logger, WifiManager& wifi);
+  void begin(AsyncWebServer& server); void update(); void broadcastReading(const RadarReading& reading); void broadcastStatus(); bool cloudConnected() const{return _cloudConnected;}
+ private:
+  void onLocalEvent(AsyncWebSocket*,AsyncWebSocketClient*,AwsEventType,void*,uint8_t*,size_t);
+  void handleIncoming(const uint8_t*,size_t,AsyncWebSocketClient*,bool); void handleCommand(const String&,AsyncWebSocketClient*,bool); void handleSettings(JsonObjectConst,AsyncWebSocketClient*,bool);
+  void sendConfig(AsyncWebSocketClient* client=nullptr,bool cloudOnly=false); void sendAck(const String&,AsyncWebSocketClient*,bool); void sendError(const String&,AsyncWebSocketClient*,bool); void exportLogs(AsyncWebSocketClient*,bool); void sendText(const String&,AsyncWebSocketClient*,bool);
+  void syncCloud(); void startCloud(); void stopCloud(); static void cloudEventRouter(WStype_t,uint8_t*,size_t); void onCloudEvent(WStype_t,uint8_t*,size_t);
+  static RadarWebSocket* _instance; AsyncWebSocket _localWs; WebSocketsClient _cloudWs; Radar& _radar; Alarm& _alarm; Logger& _logger; WifiManager& _wifi; bool _cloudInitialized=false,_cloudConnected=false; uint32_t _lastCloudAttemptMs=0; String _cloudHostSnapshot,_cloudDeviceSnapshot,_cloudTokenSnapshot; bool _cloudEnabledSnapshot=false;
+};
