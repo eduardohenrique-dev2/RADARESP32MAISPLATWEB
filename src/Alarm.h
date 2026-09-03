33867ca0b1ab423aea49f30a3f80c17d553848b9
Eduardo Henrique Gonçalves <eduardohenriquegoncalves12@gmail.com>
2026-09-02 21:32:48 -0300

    feat: convert alarm LED driver to WS2812 RGB

--- src/Alarm.h
+++ src/Alarm.h
@@ -1,4 +1,46 @@
 #pragma once
+
 #include <Arduino.h>
-struct AlarmTransition{bool active=false;bool activated=false;bool cleared=false;};
-class Alarm{public:void begin(uint8_t buzzerPin,uint8_t ledPin);void updateOutputs();AlarmTransition process(float distanceCm,bool valid);void clear();void setDetectionEnabled(bool enabled);void setBuzzerEnabled(bool enabled);void setLedEnabled(bool enabled);void setManualLed(bool on);void setThreshold(float thresholdCm);bool active()const{return _active;}bool detectionEnabled()const{return _detectionEnabled;}bool buzzerEnabled()const{return _buzzerEnabled;}bool ledEnabled()const{return _ledEnabled;}private:void applyOutputs(bool buzzerOn,bool ledOn);uint8_t _buzzerPin=255,_ledPin=255;bool _active=false,_detectionEnabled=true,_buzzerEnabled=true,_ledEnabled=true,_manualLed=false;float _thresholdCm=30.0f,_hysteresisCm=5.0f;uint32_t _lastAlarmStateChangeMs=0;};
+#include <Adafruit_NeoPixel.h>
+
+struct AlarmTransition {
+  bool active = false;
+  bool activated = false;
+  bool cleared = false;
+};
+
+class Alarm {
+ public:
+  void begin(uint8_t buzzerPin, uint8_t ledPin);
+  void updateOutputs();
+  AlarmTransition process(float distanceCm, bool valid);
+  void clear();
+  void setDetectionEnabled(bool enabled);
+  void setBuzzerEnabled(bool enabled);
+  void setLedEnabled(bool enabled);
+  void setManualLed(bool on);
+  void setThreshold(float thresholdCm);
+
+  bool active() const { return _active; }
+  bool detectionEnabled() const { return _detectionEnabled; }
+  bool buzzerEnabled() const { return _buzzerEnabled; }
+  bool ledEnabled() const { return _ledEnabled; }
+
+ private:
+  void applyBuzzer(bool on);
+  void setRgb(uint8_t red, uint8_t green, uint8_t blue);
+
+  uint8_t _buzzerPin = 255;
+  uint8_t _ledPin = 255;
+  Adafruit_NeoPixel _pixel{1, -1, NEO_RGB + NEO_KHZ800};
+
+  bool _active = false;
+  bool _detectionEnabled = true;
+  bool _buzzerEnabled = true;
+  bool _ledEnabled = true;
+  bool _manualLed = false;
+
+  float _thresholdCm = 30.0f;
+  float _hysteresisCm = 5.0f;
+  uint32_t _lastAlarmStateChangeMs = 0;
+};
